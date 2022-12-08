719d0a7f7ae4534a52b0ee85d6d2f318d667276b
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2022-12-08 13:56:12 +0600

    sol

--- Divisible Sum Pairs.js
+++ Divisible Sum Pairs.js
@@ -34,6 +34,17 @@ function readLine() {
 
 function divisibleSumPairs(n, k, ar) {
     // Write your code here
+    let sum = 0;
+    let pairs = 0;
+    for(let i = 0; i <n; i++) {
+        for(let j = i+1; j<n; j++){
+            sum = ar[i]+ar[j];
+            if(sum% k === 0){
+                pairs +=1;
+            }
+        }
+    }
+    return pairs
 
 }
 
