036a243710be16911582f7f94e64c44430573f21
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2022-12-12 19:09:54 +0600

    answer can be more efficeint

--- Migratory Birds.js
+++ Migratory Birds.js
@@ -31,10 +31,42 @@ function readLine() {
 
 function migratoryBirds(arr) {
     // Write your code here
-    const birdTypes = Array(6).fill(0)
-    
+   
+    // count the amoun tof occurences of each number
+
+    // create object
+    const obj = {}
+    // loop over array
+    arr.forEach(number => {
+        // for each number in array,
+        // if it doesn't aleady exist as a jet ib the
+        // object, create one and set its value to 1
+        if(!obj[number]) {
+            obj[number] = 1;
+        } else{
+            //if it already exists as key on the object,
+            // increment is corresponding value.
+            obj[number] += 1;
+        }
+    });
+
+    // return object key wih highest value.
+    let highestValue = 0;
+    let highestValueKey = -Infinity;
+
+    for (let key in obj) {
+        const value = obj[key];
+        if(value>highestValue){
+            highestValue = value;
+            highestValueKey = key
+        }
+    }
+    // convert key back to number
+    return Number(highestValueKey)
+
 
 }
+
 
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
