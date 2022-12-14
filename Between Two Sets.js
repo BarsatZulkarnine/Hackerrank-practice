3c1c2ecb6096cf5b8e54e9a1133feb02e50c1916
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2022-12-14 12:39:35 +0600

    first attempt

--- Between Two Sets.js
+++ Between Two Sets.js
@@ -33,8 +33,40 @@ function readLine() {
 
 function getTotalX(a, b) {
     // Write your code here
+    const obj = {}
+    // loop over array
+    array.forEach(number => {
+        // for each number in array,
+        // if it doesn't aleady exist as a jet ib the
+        // object, create one and set its value to 1
+        if(!obj[number]) {
+            obj[number] = 1;
+        } else{
+            //if it already exists as key on the object,
+            // increment is corresponding value.
+            obj[number] += 1;
+        }
+    });
 
+    // return object key wih highest value.
+    let highestValue = 0;
+    let highestValueKey = -Infinity;
+
+  /*  for (let key in obj) {
+        const value = obj[key];
+        if(value>highestValue){
+            highestValue = value;
+            highestValueKey = key
+        }
+    }
     
+    */
+   let pairs = 0
+
+    for (let key in obj){
+        const value = obj[key];
+        pairs+= value % 2
+    }
 
 }
 
