cb65e2c5f660a681773e8706367cdc3f1d5078d5
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2023-01-04 20:15:15 +0600

    done

--- Easy problems/Drawing Book.js
+++ Easy problems/Drawing Book.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -32,48 +32,32 @@ function readLine() {
  */
 
 function pageCount(n, p) {
-    // Write your code here
-    let count = 0;
-    let mid = Math.floor(n/2)
-    if(p = 1){
-        return 1
-    }
-    if(p<= mid){
-        count = 1
-        for(let i = 0; i <= mid; ) {
-            count+= 1
-            if(i >= p){
-                break
-            }
-        }
-        i+=2
-    }
-    else{
-        for(let i = n; i >= mid;) {
-            
-            if((i == p) || (i-1 == p)){
-                break
-            }
-            count+= 1
-            i-=2
-        }
-    }
-    return count
+  // Write your code here
+  let frontFlip = 0;
+  let backFlip = 0;
 
-
-    
+  if (n != p) {
+    frontFlip = Math.floor(p / 2);
+    if (p % 2 == 1) {
+      backFlip = Math.floor((n - (p - 1)) / 2);
+    } else {
+      backFlip = Math.floor((n - p) / 2);
+    }
+    return Math.min(frontFlip, backFlip);
+  }
+  return 0;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const n = parseInt(readLine().trim(), 10);
+  const n = parseInt(readLine().trim(), 10);
 
-    const p = parseInt(readLine().trim(), 10);
+  const p = parseInt(readLine().trim(), 10);
 
-    const result = pageCount(n, p);
+  const result = pageCount(n, p);
 
-    ws.write(result + '\n');
+  ws.write(result + "\n");
 
-    ws.end();
+  ws.end();
 }
